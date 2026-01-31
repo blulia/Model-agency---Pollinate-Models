0404c4f6cc55a091382fd8bb3bb258d9522ba34d
Yuliia Burei <152267398+blulia@users.noreply.github.com>
2026-01-31 23:38:32 +0200

    Update backup.sql

--- pollinate-models/backup.sql
+++ pollinate-models/backup.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
 --
--- Host: localhost    Database: models_database
+-- Host: 127.0.0.1    Database: models_database
 -- ------------------------------------------------------
--- Server version	8.0.40
+-- Server version	8.0.44
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -60,7 +60,7 @@ CREATE TABLE `already_models` (
   `shoe` int NOT NULL,
   `photo_url` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,8 +69,65 @@ CREATE TABLE `already_models` (
 
 LOCK TABLES `already_models` WRITE;
 /*!40000 ALTER TABLE `already_models` DISABLE KEYS */;
-INSERT INTO `already_models` VALUES (1,'Daria','B','+380671234567',177,78,60,91,39,'\\uploads_already\\Daria_B'),(2,'Lilith','L','+380959876543',175,88,61,89,39,'\\uploads_already\\Lilith_L'),(3,'Anisia','A','+380634567890',172,75,60,88,38,'\\uploads_already\\Anisia_A'),(4,'Soni','D','+380681122334',176,79,63,89,39,'\\uploads_already\\Soni_D'),(5,'Artur','H','+380735566778',184,98,75,91,44,'\\uploads_already\\Artur_H'),(6,'Dima','T','+380993344556',187,96,76,94,45,'\\uploads_already\\Dima_T'),(7,'Borys','D','+380667788990',185,95,75,98,43,'\\uploads_already\\Borys_D'),(8,'Danyil','D','+380936677889',182,92,77,99,43,'\\uploads_already\\Danyil_D'),(9,'Vlad','Tobias','576184395',180,70,50,70,37,'\\uploads\\Vlad_Tobias');
+INSERT INTO `already_models` VALUES (1,'Daria','B','+380671234567',178,78,60,91,39,'\\uploads_already\\Daria_B'),(2,'Lilith','L','+380959876543',175,88,61,89,39,'\\uploads_already\\Lilith_L'),(3,'Anisia','A','+380634567890',172,75,60,88,38,'\\uploads_already\\Anisia_A'),(4,'Soni','D','+380681122334',176,79,63,89,39,'\\uploads_already\\Soni_D'),(5,'Artur','H','+380735566778',184,98,75,91,44,'\\uploads_already\\Artur_H'),(6,'Dima','T','+380993344556',187,96,76,94,45,'\\uploads_already\\Dima_T'),(7,'Borys','D','+380667788990',185,95,75,98,43,'\\uploads_already\\Borys_D'),(8,'Danyil','D','+380936677889',182,92,77,99,43,'\\uploads_already\\Danyil_D');
 /*!40000 ALTER TABLE `already_models` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chats`
+--
+
+DROP TABLE IF EXISTS `chats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chats` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `photographer_id` int NOT NULL,
+  `model_id` int NOT NULL,
+  `photosession_id` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_chat` (`photographer_id`,`model_id`,`photosession_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chats`
+--
+
+LOCK TABLES `chats` WRITE;
+/*!40000 ALTER TABLE `chats` DISABLE KEYS */;
+INSERT INTO `chats` VALUES (1,2,3,1,'2026-01-27 13:36:33');
+/*!40000 ALTER TABLE `chats` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `chat_id` int NOT NULL,
+  `text` text NOT NULL,
+  `sender_role` enum('model','photographer') NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_chat` (`chat_id`),
+  CONSTRAINT `fk_chat` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+INSERT INTO `messages` VALUES (2,1,'Hi!','photographer','2026-01-27 13:39:58'),(3,1,'Hi!','model','2026-01-27 13:49:00');
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -85,6 +142,7 @@ CREATE TABLE `model_applications` (
   `model_id` int NOT NULL,
   `session_id` int NOT NULL,
   `status` enum('pending','accepted','rejected') DEFAULT 'pending',
+  `chat_id` int DEFAULT NULL,
   `submitted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
@@ -99,8 +157,42 @@ CREATE TABLE `model_applications` (
 
 LOCK TABLES `model_applications` WRITE;
 /*!40000 ALTER TABLE `model_applications` DISABLE KEYS */;
-INSERT INTO `model_applications` VALUES (1,5,1,'accepted','2025-05-20 07:14:38','Daria','B');
+INSERT INTO `model_applications` VALUES (1,3,1,'pending',NULL,'2026-01-27 13:11:45','Daria','B');
 /*!40000 ALTER TABLE `model_applications` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `model_updates`
+--
+
+DROP TABLE IF EXISTS `model_updates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `model_updates` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` int NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `surname` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `height` int DEFAULT NULL,
+  `bust` int DEFAULT NULL,
+  `waist` int DEFAULT NULL,
+  `hips` int DEFAULT NULL,
+  `shoe` int DEFAULT NULL,
+  `status` enum('pending','approved','rejected') DEFAULT 'pending',
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `model_updates`
+--
+
+LOCK TABLES `model_updates` WRITE;
+/*!40000 ALTER TABLE `model_updates` DISABLE KEYS */;
+INSERT INTO `model_updates` VALUES (1,3,'Daria','B','+380671234567',176,78,60,91,39,'approved','2026-01-09 12:50:52'),(2,3,'Daria','B','+380671234567',177,78,60,91,39,'approved','2026-01-09 14:28:34'),(3,3,'Daria','B','+380671234567',178,78,60,91,39,'approved','2026-01-14 15:04:21'),(4,3,'Daria','B','+380671234567',177,78,60,91,39,'pending','2026-01-27 14:20:32');
+/*!40000 ALTER TABLE `model_updates` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -129,7 +221,7 @@ CREATE TABLE `models` (
   `photos_path` text NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -138,7 +230,7 @@ CREATE TABLE `models` (
 
 LOCK TABLES `models` WRITE;
 /*!40000 ALTER TABLE `models` DISABLE KEYS */;
-INSERT INTO `models` VALUES (11,'Bohdan','Yellow',21,'Kryvyi Rih','+380985146772',190,98,80,105,44,'blue','black','https://www.instagram.com/bodya_yo','','\\uploads\\Bohdan_Yellow','2025-05-20 06:47:10');
+INSERT INTO `models` VALUES (1,'Anton','Grinch',22,'Warsaw','+48576184395',180,89,70,92,40,'brown','black','https://www.instagram.com/antony_grinch','','\\uploads\\Anton_Grinch','2026-01-27 19:19:03');
 /*!40000 ALTER TABLE `models` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -177,10 +269,11 @@ DROP TABLE IF EXISTS `sessions`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `sessions` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `photographer_id` int NOT NULL,
   `title` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `date` datetime NOT NULL,
-  `location` varchar(255) NOT NULL,
+  `description` text,
+  `date` date DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -192,7 +285,7 @@ CREATE TABLE `sessions` (
 
 LOCK TABLES `sessions` WRITE;
 /*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES (1,'Lviv fairies','Photoshoot in the city center in fairy costumes.','2025-05-30 00:00:00','Lviv','2025-05-19 20:39:13'),(2,'In the yard with a basket of fruit ','A warm, natural-themed photoshoot set in a cozy backyard. Surrounded by greenery and soft sunlight, the model poses with a rustic fruit basket, creating a relaxed, countryside vibe that highlights freshness, simplicity, and natural beauty.','2025-06-01 00:00:00','Kryvyi Rih','2025-05-20 07:08:35');
+INSERT INTO `sessions` VALUES (1,2,'Sping orangery - fairy','Photoshoot in orangery in fairy and mystic style','2026-04-27','Main Orangery of Kryvyi Rig','2026-01-27 13:00:25'),(2,2,'Lviv fairies','Photoshoot in the city center in fairy costumes','2026-03-16','Lviv','2026-01-27 17:58:26');
 /*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -212,7 +305,7 @@ CREATE TABLE `users` (
   `role` enum('model','photographer','admin') DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `phone` (`phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -221,7 +314,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Vlad','Tobias','576184395','$2b$10$uCM/cA/fIgnWXzlMpkGqUOQaGtRE1szDbC0cjFOBWeOIwjCmD9sZW','model'),(2,'Vlada','Kozobrod','+380152677160','$2b$10$ck0OuuYUauA6/xBdQm29u.Kt8dsrotWni/D5t4dvmNOikZFyr0z5O','photographer'),(3,'Yuliia','Burei','+380677160152','$2b$10$ReJabbRpJUefCYPaePKeD.keweJ4MLA6qquYbprASGfehTMPsdvpm','admin'),(4,'Artur','H','+380735566778','$2b$10$rB8Ffg7Z6h27XD4uQK4SkOsRqWVHrV07AbgTNz.V4OlNhLbEJcWsK','model'),(5,'Daria','B','+380671234567','$2b$10$pGZrFizR1fXscSmp5XXLveX2FPTSt36bnM5mZT4FsXsBdmfW57UMy','model');
+INSERT INTO `users` VALUES (1,'Yuliia','Burei','+380677160152','$2b$10$8y5mVDHAihicvdcS1M/Tt.J9UyQZogEUDu6NkMP4H0Y8NClsPpbii','admin'),(2,'Vlada','Kozobrod','+380152677160','$2b$10$y/Sv6UX7LaJUxGOTyhH8sO4ul4uSrmergSPSGhlMkN2XD0MT6E5pa','photographer'),(3,'Daria','B','+380671234567','$2b$10$pgEpyTBC04rBaB7UfgpDKe0336LynRM66vwqcCNzhB7ItrS7nUTHC','model');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -234,4 +327,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-31 23:49:06
+-- Dump completed on 2026-01-31 23:36:02
